c8b59e184558f0c8b326173ec463a6d132e22c56
heewookji <wlgmldnr@daum.net>
2021-07-12 22:15:37 +0900

    feat: use rand crate, compare user input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,6 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -6,10 +8,21 @@ fn main() {
 
     let mut guess = String::new();
 
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is : {}", secret_number);
+
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line!");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Equal => println!("You win"),
+        Ordering::Greater => println!("Too big!"),
+    }
 
 }
