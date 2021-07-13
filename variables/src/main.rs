6c6f14fd56cf0f0e12ed0e61e568683b604c385a
heewookji <wlgmldnr@daum.net>
2021-07-13 22:32:58 +0900

    feat: finish variables, shadow, immutable

---
+++ variables/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+
+    let spaces = "     ";
+    let spaces = spaces.len();
+
+    println!("spaces is : {}", spaces);
+}
+
