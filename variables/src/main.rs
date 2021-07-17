96962248bf62077dc6cf18d68461c58ba190112f
heewookji <wlgmldnr@daum.net>
2021-07-17 17:48:10 +0900

    feat: tuple, array

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let mut x = 5;
     println!("The value of x is: {}", x);
@@ -13,5 +15,40 @@ fn main() {
     let spaces = spaces.len();
 
     println!("spaces is : {}", spaces);
-}
 
+    let literals = b'B' - b'A';
+    println!("{}", literals);
+
+    let x = 2.0;
+    let y: f32 = 3.0;
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {}", y);
+    println!("The value of x is: {}", tup.0);
+
+    let mut array = [3; 5];
+    array = [1, 2, 3, 4, 5];
+
+    println!("The first value of array is: {}", array[0]);
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = array[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    )
+
+}
